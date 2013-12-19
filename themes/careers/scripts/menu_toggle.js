e4c67add5e858540463445eb7a1c5bb6e31f228d
Brian Dickey <brian-dickey@uiowa.edu>
2013-12-19 13:09:43 -0600

    added js for menu toggle

---
+++ themes/careers/scripts/menu_toggle.js
@@ -0,0 +1,24 @@
+(function ($) {
+   $(document).ready(function() {
+    $(".nolink").addClass("menu-toggle").attr("tabindex",0);
+    if ($(".menu-name-menu-audience-menu li").hasClass("active-trail")) {
+      $(".menu-name-menu-audience-menu .active-trail").parent("ul").removeClass("hide").addClass("show").show();
+    }
+    else {
+      $(".menu-toggle").next("ul").addClass("hide").hide();
+    }
+
+
+    // menu-toggle onClick event
+    $(".menu-toggle").bind("click", function() {
+      if ($(this).next("ul").hasClass("hide")) {
+        $(this).next("ul").removeClass("hide").addClass("show").show();
+      }
+      else {
+        $(this).next("ul").removeClass("show").addClass("hide").hide();
+      }
+    });
+
+
+   });
+}(jQuery));
