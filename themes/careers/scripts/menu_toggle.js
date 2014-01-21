a308c5d5196b7ae53163af4c6005056a0217b72d
Jalessa <jalessa-hall@uiowa.edu>
2014-01-21 16:50:52 -0600

    adjusted expand/collapsed menu toggle
    Signed-off-by: Jalessa <jalessa-hall@uiowa.edu>

--- themes/careers/scripts/menu_toggle.js
+++ themes/careers/scripts/menu_toggle.js
@@ -2,6 +2,12 @@
    $(document).ready(function() {
     $(".nolink").addClass("menu-toggle is-collapsed").attr("tabindex",0);
     $(".menu-toggle").parent().next("ul").addClass("hide").hide();
+
+    //We are assuming all list items are collapsed unless the list items are part of the menu's active trail
+    //Resolves the issue of expanding all children items in sidebar menu blocks
+    $("ul.menu li.expanded").removeClass("expanded").addClass("collapsed");
+    $("ul.menu li.active-trail").removeClass("collapsed").addClass("expanded");
+
     if ($(".block-region-sidebar-first .menu-name-menu-audience-menu li").hasClass("active-trail")) {
       $(".block-region-sidebar-first .menu-name-menu-audience-menu .active-trail").parent("ul").removeClass("hide").addClass("show").show();
       $("ul.show").siblings(".menu-inner").children(".menu-toggle").removeClass("is-collapsed").addClass("is-expanded");
