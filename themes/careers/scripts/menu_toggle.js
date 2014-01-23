ecea3c4248f3a00459f57659148880d566108287
Adam Delaney <adam-delaney@uiowa.edu>
2014-01-23 10:49:41 -0600

    refactored menu-toggle js to improve code effeciency and scoping

--- themes/careers/scripts/menu_toggle.js
+++ themes/careers/scripts/menu_toggle.js
@@ -1,34 +1,30 @@
 (function ($) {
-   $(document).ready(function() {
-    $(".nolink").addClass("menu-toggle is-collapsed").attr("tabindex",0);
-    $(".menu-toggle").parent().next("ul").addClass("hide").hide();
+    $(document).ready(function() {
+      // variables
+      var expandMenus = $('#block-menu-block-3');
+      var menuToggles = expandMenus.find('span.nolink');
+      var menuToggleMenus = menuToggles.closest('li').children('ul.menu');
+      var menuListItems = expandMenus.find('li');
 
-    //We are assuming all list items are collapsed unless the list items are part of the menu's active trail
-    //Resolves the issue of expanding all children items in sidebar menu blocks
-    $("ul.menu li.expanded").removeClass("expanded").addClass("collapsed");
-    $("ul.menu li.active-trail").removeClass("collapsed").addClass("expanded");
+      // set default states
+      menuToggles.addClass('menu-toggle is-collapsed').attr('tabindex', 0);
+      menuToggleMenus.addClass('is-expandable is-hidden').hide();
 
-    if ($(".block-region-sidebar-first .menu-name-menu-audience-menu li").hasClass("active-trail")) {
-      $(".block-region-sidebar-first .menu-name-menu-audience-menu .active-trail").parent("ul").removeClass("hide").addClass("show").show();
-      $("ul.show").siblings(".menu-inner").children(".menu-toggle").removeClass("is-collapsed").addClass("is-expanded");
-    }
-    else {
-      $(".menu-toggle").parent().next("ul").addClass("hide").hide();
-    }
+      // check for li attributes
+      menuListItems.each(function() {
+        if($(this).hasClass('expanded')) {
+          $(this).removeClass('expanded').addClass('collapsed');
+        }
+        if($(this).hasClass('active-trail')) {
+          $(this).removeClass('collapsed').addClass('expanded');
+          $(this).parent('ul').toggleClass('is-hidden is-visible').show();
+          $(this).parent('ul.is-expandable').siblings('.menu-inner').find('.menu-toggle').removeClass('is-collapsed').addClass('is-expanded');
+        }
+      });
 
-
-    // menu-toggle onClick event
-    $(".menu-toggle").bind("click", function() {
-      if ($(this).parent().next("ul").hasClass("hide")) {
-        $(this).parent().next("ul").removeClass("hide").addClass("show").show();
-        $(this).removeClass("is-collapsed").addClass("is-expanded");
-      }
-      else {
-        $(this).parent().next("ul").removeClass("show").addClass("hide").hide();
-        $(this).removeClass("is-expanded").addClass("is-collapsed");
-      }
+      // menuToggles event handler
+      $('span.menu-toggle').bind('click', function() {
+        $(this).toggleClass('is-collapsed is-expanded').closest('li').find('ul.is-expandable').toggle().toggleClass('is-hidden is-visible');
+      });
     });
-
-
-   });
 }(jQuery));
