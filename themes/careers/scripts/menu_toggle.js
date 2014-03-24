265fa1b8d4f67b9cfba07a18151bb25288ed7015
Joe Whitsitt <joe-whitsitt@uiowa.edu>
2014-03-24 14:49:18 -0500

    first attempt at menu accessibility.
    Signed-off-by: Joe Whitsitt <joe-whitsitt@uiowa.edu>

--- themes/careers/scripts/menu_toggle.js
+++ themes/careers/scripts/menu_toggle.js
@@ -6,9 +6,16 @@
       var menuToggleMenus = menuToggles.closest('li').children('ul.menu');
       var menuListItems = expandMenus.find('li');
 
-      // set default states
-      menuToggles.addClass('menu-toggle is-collapsed').attr('tabindex', 0);
-      menuToggleMenus.addClass('is-expandable is-hidden').hide();
+      // set default accessibilty states
+      expandMenus.attr('role','tablist');
+      menuToggles.addClass('menu-toggle is-collapsed').attr('tabindex', 0).attr('role','tab');
+      menuToggleMenus.addClass('is-expandable is-hidden').attr('aria-expanded',false).attr('role','tabpanel').hide();
+
+      // assign accessible relationship between toggler and toggler content
+      menuToggles.each(function(i) {
+        $(this).attr('id', 'tab'+(i+1));
+        $(this).closest('li').children('ul.menu').attr('aria-labelledby', 'tab'+(i+1));
+      });
 
       // check for li attributes
       menuListItems.each(function() {
@@ -21,6 +28,13 @@
       // menuToggles event handler
       $('span.menu-toggle').bind('click', function() {
         $(this).toggleClass('is-collapsed is-expanded').closest('li').find('ul.is-expandable').toggle().toggleClass('is-hidden is-visible');
+
+        // change accessbility attribute based state.
+        if ($(this).hasClass('is-expanded')) {
+          $(this).closest('li').find('ul.is-expandable').attr('aria-expanded',true);
+        } else {
+          $(this).closest('li').find('ul.is-expandable').attr('aria-expanded',false);
+        };
       });
     });
 }(jQuery));
