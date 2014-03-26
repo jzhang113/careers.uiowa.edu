e666e18d3b2db0440610e405d55e959621f02b7f
Joe Whitsitt <joe-whitsitt@uiowa.edu>
2014-03-26 14:53:27 -0500

    Add aria-labelledby attribute
    Signed-off-by: Joe Whitsitt <joe-whitsitt@uiowa.edu>

--- themes/careers/scripts/collapsible_aria.js
+++ themes/careers/scripts/collapsible_aria.js
@@ -7,19 +7,23 @@
   	// set default attributes
   	$collapsible.each(function() {
   		$(this).find('a:first').attr('role','tab');
-  		$(this).find('div:first').attr('role','tabpanel');
+
+      var $collapsibleID = $(this).attr('id'); 
+      var $wrapperDiv = $(this).find('div:first');
+
+  		$wrapperDiv.attr('role','tabpanel').attr('aria-labelledby', $collapsibleID);
 
     	if ($(this).hasClass('collapsed')) {
-    		$(this).find('div:first').attr('aria-expanded',false);
+    		$wrapperDiv.attr('aria-expanded',false);
     	} else {
-    		$(this).find('div:first').attr('aria-expanded',true);
+    		$wrapperDiv.attr('aria-expanded',true);
     	};
   	});
 
   	// "toggle" aria-expanded attribute on click
   	$collapsible.find('a:first').bind( "click", function() {
+      // reusing wrapperDiv after setting default attributes
   		var $wrapperDiv = $(this).closest('.collapsible').find('div:first');
-  		
   		$wrapperDiv.attr('aria-expanded', $wrapperDiv.attr('aria-expanded')=='false' ? 'true' : 'false');
 		});
 
