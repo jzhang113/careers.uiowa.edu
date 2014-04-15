13b31ad64bd8b5fee80cae30b55cea5d98c1f2a8
Joe Whitsitt <joe-whitsitt@uiowa.edu>
2014-04-15 09:29:10 -0500

    ARIA Tree added.
    Signed-off-by: Joe Whitsitt <joe-whitsitt@uiowa.edu>

--- themes/careers/scripts/menu_toggle.js
+++ themes/careers/scripts/menu_toggle.js
@@ -2,44 +2,49 @@
     $(document).ready(function() {
       // variables
       var expandMenus = $('#block-menu-block-3, #block-menu-block-6');
+      var menuTree = expandMenus.find('.menu-block-wrapper').children('ul.menu:first');
       var menuToggles = expandMenus.find('span.nolink');
       var menuToggleMenus = menuToggles.closest('li').children('ul.menu');
       var menuListItems = expandMenus.find('li');
-      var menuTabs = [];  // For 'aria-owns' attribute
+
+      // toggle attribute function
+      $.fn.toggleAttr = function(attr, attr1, attr2) {
+        return this.each(function() {
+          var self = $(this);
+          if (self.attr(attr) == attr1)
+            self.attr(attr, attr2);
+          else
+            self.attr(attr, attr1);
+        });
+      };
 
       // set default states
-      expandMenus.attr('role','tablist');
-      menuToggles.addClass('menu-toggle is-collapsed').attr('tabindex', 0).attr('role','tab');
-      menuToggleMenus.addClass('is-expandable is-hidden').attr('role','tabpanel').attr('aria-expanded',false).hide();
+      menuTree.attr('role','tree');
+      menuListItems.attr('role','presentation');
+      expandMenus.find('li.expanded').attr('aria-expanded','false');
+      menuToggles.addClass('menu-toggle is-collapsed').attr('tabindex', 0).attr('role','treeitem');
+      menuToggleMenus.addClass('is-expandable is-hidden').attr('role','group').attr('aria-hidden','true').hide();
 
-      // assign accessible relationship between toggler and toggler content
-      menuToggles.each(function(i) {
-        $(this).attr('id', 'tab'+(i+1));
-        $(this).closest('li').children('ul.menu').attr('aria-labelledby', 'tab'+(i+1));
-        menuTabs[menuTabs.length] = $(this).attr('id');
+      // Add group ids and aria-owns info
+      menuToggleMenus.each(function(i) {
+        $(this).attr('id', 'group'+(i+1));
+        $(this).parent('li').attr('aria-owns','group'+(i+1));
       });
 
-      // assign ids to aria-owns attribute
-      expandMenus.attr('aria-owns', menuTabs.join(" "));
-
-      // check for li attributes
+      // check for li active trail and change classes and attributes
       menuListItems.each(function() {
         if($(this).hasClass('active-trail')) {
-          $(this).parent('ul').toggleClass('is-hidden is-visible').attr('aria-expanded',true).show();
+          $(this).attr('aria-expanded', 'true');
+          $(this).parent('ul').toggleClass('is-hidden is-visible').attr('aria-hidden','false').show();
           $(this).parent('ul.is-expandable').siblings('.menu-inner').find('.menu-toggle').removeClass('is-collapsed').addClass('is-expanded');
         }
       });
 
       // menuToggles event handler
       $('span.menu-toggle').bind('click', function() {
-        $(this).toggleClass('is-collapsed is-expanded').closest('li').find('ul.is-expandable').toggle().toggleClass('is-hidden is-visible');
-
-        // change accessbility attribute based state.
-        if ($(this).hasClass('is-expanded')) {
-          $(this).closest('li').find('ul.is-expandable').attr('aria-expanded',true);
-        } else {
-          $(this).closest('li').find('ul.is-expandable').attr('aria-expanded',false);
-        };
+        //change classes and attributes on click
+        $(this).toggleClass('is-collapsed is-expanded').closest('li').find('ul.is-expandable:first').toggle().toggleClass('is-hidden is-visible').toggleAttr('aria-hidden', 'true', 'false');
+        $(this).closest('li.expanded').toggleAttr('aria-expanded', 'true', 'false');
       });
     });
 }(jQuery));
